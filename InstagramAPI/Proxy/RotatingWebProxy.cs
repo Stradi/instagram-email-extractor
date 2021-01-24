553cb99f9760b30438d069f8f67b41981c917cd6
Batin EVIRGEN <batinevirgen@gmail.com>
2021-01-24 17:26:13 +0300

    Updated RotatingWebProxy.cs file for the future.

--- InstagramAPI/Proxy/RotatingWebProxy.cs
+++ InstagramAPI/Proxy/RotatingWebProxy.cs
@@ -19,6 +19,12 @@ namespace InstagramAPI.Proxy {
     }
 
     public Uri GetProxy(Uri destination) {
+      return new Uri(GetAvailableProxy());
+    }
+
+    // Added this function because in future we may want to choose proxy with another algorithm
+    // such as checking request count made by proxy to avoid IP bans etc.
+    private string GetAvailableProxy() {
       string proxy = string.Format(
         "http://{0}:{1}",
         proxies[currentProxyIndex].ipAddress,
@@ -29,8 +35,8 @@ namespace InstagramAPI.Proxy {
       if(currentProxyIndex >= proxies.Count) {
         currentProxyIndex = 0;
       }
-      
-      return new Uri(proxy);
+
+      return proxy;
     }
 
     public bool IsBypassed(Uri host) {
