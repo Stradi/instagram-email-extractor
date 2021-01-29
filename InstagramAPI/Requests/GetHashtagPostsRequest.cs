95e473fef0f000228f8d11f1b51a09592960248a
Batin EVIRGEN <batinevirgen@gmail.com>
2021-01-29 17:37:06 +0300

    Removed forgotten '}' in variablesParams that causes crash.

--- InstagramAPI/Requests/GetHashtagPostsRequest.cs
+++ InstagramAPI/Requests/GetHashtagPostsRequest.cs
@@ -5,9 +5,9 @@ namespace InstagramAPI.Requests {
         this.url = Constants.BASE_URL + string.Format("/explore/tags/{0}/?__a=1", hashtagName);
       } else {
         this.url = Constants.GRAPHQL_URL;
-        
+
         string variablesParams = "";
-        variablesParams = string.Format("{\"tag_name\":\"{0}\",\"first\":24},\"after\":\"{1}\"", hashtagName, endCursor);
+        variablesParams = string.Format("{{\"tag_name\":\"{0}\",\"first\":24,\"after\":\"{1}\"}}", hashtagName, endCursor);
 
         this.parameters = new RequestParams[] {
           new RequestParams() { name = "query_hash", value = Constants.HASHTAG_POSTS_QUERY_HASH },
