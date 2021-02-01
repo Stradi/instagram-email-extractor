ca2df925fdb284287936c97e6885c4ca6ceb1622
Batin EVIRGEN <batinevirgen@gmail.com>
2021-02-01 20:43:37 +0300

    Added Extractor page and divided screen to 3 equal parts.

---
+++ InstagramEmailExtractor/Extractor.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InstagramEmailExtractor {
+  /// <summary>
+  /// Interaction logic for Extractor.xaml
+  /// </summary>
+  public partial class Extractor : Page {
+    public Extractor() {
+      InitializeComponent();
+    }
+  }
+}
